24a760c50bb8ee702ddf35914e133cd0a910e71a
klentpimentel22 <161108051+klentpimentel22@users.noreply.github.com>
2024-03-18 00:20:13 +0800

    Add files via upload

---
+++ services (1).java
@@ -0,0 +1,29 @@
+// ServiceDAO.java
+
+import java.util.List;
+
+public class services {
+
+    public void addService(Service service) {
+        // Implement adding a service to the database
+    }
+
+    public List<Service> getAllServices() {
+        // Implement fetching all services from the database
+        return null;
+    }
+
+    public Service getServiceById(int id) {
+        // Implement fetching a service by ID from the database
+        return null;
+    }
+
+    public void updateService(Service service) {
+        // Implement updating a service in the database
+    }
+
+    public void deleteService(int id) {
+        // Implement deleting a service from the database
+    }
+}
+
